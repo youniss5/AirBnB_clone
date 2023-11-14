6d97364181c186dd64e80c038eef746a2cd117c5
youniss5 <ahmedhyouniss@gmail.com>
2023-11-14 07:37:30 -0800

    Airbnb

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" module for city class"""
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """ city class"""
-    state_id = ""
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" module for state"""
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ class of state"""
-    name = ""
